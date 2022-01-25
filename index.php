176d0a8627e82388bdb7e9524eb21d644039b0c0
quyetcoder2k3 <98365018+quyetcoder2k3@users.noreply.github.com>
2022-01-25 07:25:37 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" name="viewport">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-touch-fullscreen" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="default">
+<title>Trang Chủ </title>
+<meta name="csrf-token" content="Pxdu07BpfUC2fG78pLqROpQeqTyare5vUmKSfni1">
+<meta name="copyright" content="Bùi Văn Quyết" />
+<meta name="robots" content="index, follow" />
+<meta name="revisit-after" content="1 days" />
+<meta http-equiv="content-language" content="vi" />
+<meta property="og:url" content="trieusub.vn" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="Hệ thống dịch vụ mạng xã hội Facebook | Instagram | Youtube | Tiktok | TrieuSub.Vn" />
+<meta property="og:description" content="TrieuSub.Vn là hệ thống mua bán các dịch vụ mạng xã hội uy tín và chất lượng tại thị trường Việt Nam" />
+<meta property="og:image" content="/assets/images/trieusub123.png" />
+<meta property="fb:app_id" content="" />
+<meta name="twitter:title" content="TrieuSub.Vn là hệ thống mua bán các dịch vụ mạng xã hội uy tín và chất lượng tại thị trường Việt Nam">
+<meta name="twitter:description" content="TrieuSub.Vn là hệ thống mua bán các dịch vụ mạng xã hội uy tín và chất lượng tại thị trường Việt Nam">
